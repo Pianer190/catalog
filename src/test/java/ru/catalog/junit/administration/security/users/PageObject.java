3e43959e20b51e30b9ee1075e238bc44d8ba936e
Dmitriy Andrianov <dandrianov@srv.rbt>
2023-07-12 15:33:03 +0300

    Добавлена АТ на открытие ЭФ с проверкой верного открытия ЭФ

--- src/test/java/ru/catalog/junit/administration/security/users/PageObject.java
+++ src/test/java/ru/catalog/junit/administration/security/users/PageObject.java
@@ -7,5 +7,14 @@ class PageObject {
 
     @FindBy(xpath = "//div[contains(@id, 'common-ux-desktop-window') and contains(@class,'x-window-default-resizable')]//div[contains(@id, 'header') and contains(@class,'x-window-header-default')]//div[text() = 'Пользователи']")
     SelenideElement form_header;
-
+    @FindBy(xpath ="//div[contains(@id, 'common-ux-desktop-window') and contains(@class,'x-window-default-resizable')]//div[contains(@id, 'gridpanel') and contains(@class,'x-toolbar-default')]//*[contains(@id,'button') and (@data-qtip='Сохранить')]")
+    SelenideElement button_save;
+    @FindBy(xpath = "//div[contains(@id, 'common-ux-desktop-window') and contains(@class,'x-window-default-resizable')]//div[contains(@id, 'gridpanel') and contains(@class,'x-toolbar-default')]//*[contains(@id,'button') and (@data-qtip='Отмена')]")
+    SelenideElement button_cancel;
+    @FindBy(xpath ="//div[contains(@id, 'common-ux-desktop-window') and contains(@class,'x-window-default-resizable')]//div[contains(@id, 'gridpanel') and contains(@class,'x-toolbar-default')]//*[contains(@id,'button') and (@data-qtip='Отмена')]")
+    SelenideElement button_add;
+    @FindBy(xpath = "//div[contains(@id, 'common-ux-desktop-window') and contains(@class,'x-window-default-resizable')]//div[contains(@id, 'gridpanel') and contains(@class,'x-toolbar-default')]//*[contains(@id,'button') and (@data-qtip='Удалить')]")
+    SelenideElement button_delete;
+    @FindBy(xpath = "//div[contains(@id, 'common-ux-desktop-window') and contains(@class,'x-window-default-resizable')]//div[contains(@id, 'gridpanel') and contains(@class,'x-toolbar-default')]//*[contains(@id,'button') and (@data-qtip='Обновить')]")
+    SelenideElement button_refresh;
 }
