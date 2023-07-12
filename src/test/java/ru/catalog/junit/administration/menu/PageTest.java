9621a3358ad9aecb408e052e2f40c543bd9e5496
aubolshakov <aubolshakov@srv.rbt>
2023-07-12 13:30:34 +0300

    CATALOG22-650. Удалена проверка на появление загрузки во время запуска проекта

--- src/test/java/ru/catalog/junit/administration/menu/PageTest.java
+++ src/test/java/ru/catalog/junit/administration/menu/PageTest.java
@@ -1,23 +1,9 @@
 package ru.catalog.junit.administration.menu;
 
-
 import ru.catalog.annotations.*;
 import ru.catalog.junit.*;
 
 
 @Structure({"Администрирование", "Меню"})
 class PageTest extends BaseTest<PageSteps> {
-
-    @Users({
-            @User(login = "spoadmin", password = "12345678"),
-            @User(login = "usermo", password = "123qweASD")
-    })
-    @CsvSource(
-            {"test1,test2",
-            "test3,test4"}
-    )
-    void testOpen(String test1, String test2) {
-        System.out.println("test1 = " + test1);
-        System.out.println("test2 = " + test2);
-    }
 }
